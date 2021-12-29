96b99098e64f22b62e1b5fb931804d46fb555d1c
beyzaydin <beyzaaydin@etu.edu.tr>
2021-12-30 02:47:56 +0300

    spring boot main eklendi, readme.md kurulum eklendi

---
+++ src/test/java/ucuBirArada/project/bitirmeProjesi/BitirmeProjesiApplicationTests.java
@@ -0,0 +1,13 @@
+package ucuBirArada.project.bitirmeProjesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitirmeProjesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/tr/com/rest/main/java/ucuBirArada/project/bitirmeProjesi/BitirmeProjesiApplication.java
@@ -0,0 +1,13 @@
+package ucuBirArada.project.bitirmeProjesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitirmeProjesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitirmeProjesiApplication.class, args);
+	}
+
+}
