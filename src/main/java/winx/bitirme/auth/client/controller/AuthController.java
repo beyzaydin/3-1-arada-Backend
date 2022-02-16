1f650e03e4c493baa6eb45577c5b200dfa9a5611
beyzaaydin <beyzaaydin@etu.edu.tr>
2022-02-16 19:16:10 +0300

    Controller deneme

--- src/main/java/winx/bitirme/auth/client/controller/AuthController.java
+++ src/main/java/winx/bitirme/auth/client/controller/AuthController.java
@@ -33,7 +33,7 @@ import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/user")
 public class AuthController {
 
     private final MongoOperations mongo;
